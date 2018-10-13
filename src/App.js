c0996813f236e845385e8f3b4c6e8c6a7ec24a56
muzudre <muzudre@gmail.com>
2018-10-13 22:41:14 +0800

    Updates for auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
 
 export default class App extends Component {
+  state = {
+    loggedIn: null
+  }
 
   componentWillMount() {
     firebase.initializeApp({
@@ -20,11 +23,25 @@ export default class App extends Component {
     })
   }
 
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+
+  }
+
   render() {
     return (
       <View >
         <Header headerText="Auth" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
